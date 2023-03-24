5a3a1e511ab20f7544847ebffe549d3a45abe89a
gowtham naidu <126231952+naidu5555@users.noreply.github.com>
2023-03-24 14:01:59 +0530

    Create test25.java

---
+++ test25.java
@@ -0,0 +1,20 @@
+package filesystem;
+import java.io.*;
+import java.util.Scanner;
+public class test25 {
+
+	public static void main(String[] args) {
+		Scanner sc=new  Scanner(System.in);
+		System.out.println("enter folder name");
+		String foldername=sc.next();
+		File f=new File("C:\\"+foldername);
+		if(f.exists())
+			System.out.println("alreadsy exists");
+		else if(f.mkdir()==true)
+		System.out.println("successfully created");
+		else
+			System.out.println("cannot be created");
+
+	}
+
+}
