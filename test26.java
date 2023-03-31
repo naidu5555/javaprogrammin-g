564f128a893de77ace0cea2b28225d68f92b5e95
gowtham naidu <126231952+naidu5555@users.noreply.github.com>
2023-03-31 13:35:16 +0530

    test26 28

--- test26.java
+++ test26.java
@@ -5,10 +5,33 @@ public class test26 {
 
 	public static void main(String[] args) throws Exception{
 		Scanner sc=new  Scanner(System.in);
-		System.out.println("enter file name");
+		System.out.println("enter filename");
 		String filename=sc.next();
 		FileOutputStream output=new FileOutputStream("C:\\4cse2\\"+filename+".dat");
 		System.out.println("file created");
+		String header="name  regdno  essaysubmitted?\n";
+		output.write(header.getBytes());
+		String data1="fayaz     017     yes\n";
+		output.write(data1.getBytes());
+		String data2="anil      018     yes\n";
+		output.write(data2.getBytes());
+		String data3="ravi      019     no\n";
+		output.write(data3.getBytes());
+		String data4="fareed    020     yes\n";
+		output.write(data4.getBytes());
+		String data5="koteswar  021     no\n";
+		output.write(data5.getBytes());
+		String data6="charan    022     no\n";
+		output.write(data6.getBytes());
+		String data7="tulsi ram 022     no\n";
+		output.write(data7.getBytes());
+		String data8="vishnu    023     yes\n";
+		output.write(data8.getBytes());
+		String data9="vaarun    024     yes\n";
+		output.write(data9.getBytes());
+		String data10="yashwanth025     yes\n";
+		output.write(data10.getBytes());
+		System.out.println("file written");
 		output.close();
 
 	}
---
+++ test28.java
@@ -0,0 +1,29 @@
+//use of channel and buffer to write a FILE
+package filesystem;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.io.RandomAccessFile;
+import java.nio.*;
+public class test28 {
+
+	public static void main(String[] args) throws Exception{
+		String data="mobile-iphone 14 price-80k,Nokia 2600 price-2600";
+		RandomAccessFile file=new RandomAccessFile("C:\\gowtham\\amazon.store","rw");
+		ByteBuffer buffer=ByteBuffer.allocate(data.length());
+		//System.out.println("contents of buffer: "+buffer.getChar(2));
+		for(int i=0;i<data.length();i++) {
+			buffer.put((byte) (data.charAt(i)));
+		    
+		}
+		//for(int i=0;i<data.length();i++) {
+        //System.out.println("contents of buffer: "+(char)buffer.get(i));
+		//}
+		FileChannel filechannel=file.getChannel();
+		buffer.rewind();
+		filechannel.write(buffer);
+		System.out.println("Data written to file");
+		filechannel.close();
+		file.close();
+	}
+
+}
